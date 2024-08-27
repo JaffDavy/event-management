c8cafddf46dcbad7408d6aed90a929949cf880d5
temchelsy <148960005+temchelsy@users.noreply.github.com>
2024-08-27 15:02:15 +0100

    auth

--- backend/app.js
+++ backend/app.js
@@ -1,41 +1,48 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// import createError from 'http-errors';
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+// import eventRouter from './routes/event.js';
+import indexRouter from './routes/auth.js'
+import Joi from 'joi';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const link = Joi.link;
 
-var app = express();
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const app = express();
 
-app.use(logger('dev'));
+// Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Routes
+// app.use('/event', eventRouter);
+app.use('/auth' ,indexRouter, )
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+  res.status(404).send('Not Found');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+
+// Error handler
+app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  console.error('Error:', err);
+
   res.status(err.status || 500);
-  res.render('error');
+  res.json({ error: err.message });
 });
 
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+export default app;
